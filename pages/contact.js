0e8e7e9440c5957b1fc1c5c21847b1c2fc4ec322
Jonathan Zietsman <jonathan@metavaro.com>
2023-12-22 09:47:07 +0200

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <>
       <div className="section">
-        <div className="section-content">hi</div>
+        <div className="section-content"></div>
       </div>
     </>
   );
