88d819cb39fca20886f3d47f8cb287f81dc55926
Jonathan Zietsman <johnny.zietsman1@gmail.com>
2024-02-19 13:01:02 +0200

    page structure update

---
+++ pages/hostel/newsletter.js
@@ -0,0 +1,7 @@
+export default function HostelNewsletter() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
---
+++ pages/more/extra-information.js
@@ -0,0 +1,7 @@
+export default function ExtraInformation() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
