f7ae8fccf5cff35a05cd021a15ccea774dbbbc0b
Jonathan Zietsman <johnny.zietsman1@gmail.com>
2024-08-15 18:15:20 +0200

    Test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import fs from "fs";
+import path from "path";
 import handlebars from "handlebars";
 
 export default async function handler(req, res) {
@@ -7,6 +8,7 @@ export default async function handler(req, res) {
   const EmailFrom = process.env.FROM_EMAIL;
   const AuthUser = process.env.AUTH_USER;
   const AuthPass = process.env.AUTH_PASS;
+
   if (req.method === "POST") {
     const { name, email, message, nature } = req.body;
 
@@ -15,30 +17,9 @@ export default async function handler(req, res) {
       case "Complaints":
         toEmail = "info@lsbekker.co.za";
         break;
-      case "Compliments":
-        toEmail = "info@lsbekker.co.za";
-        break;
-      case "General Information":
-        toEmail = "info@lsbekker.co.za";
-        break;
-      case "My Account Status":
-        toEmail = "accounts@lsbekker.co.za";
-        break;
-      case "New Hostel Application":
-        toEmail = "info@lsbekker.co.za";
-        break;
-      case "New School Application":
-        toEmail = "school@lsbekker.co.za";
-        break;
-      case "Exemptions":
-        toEmail = "hr@lsbekker.com";
-        break;
-      case "Other":
-        toEmail = "info@lsbekker.co.za";
-        break;
-
+      // Other cases...
       default:
-        toEmail = EmailTo; // Default to your general email address
+        toEmail = EmailTo;
         break;
     }
 
@@ -49,35 +30,30 @@ export default async function handler(req, res) {
         pass: AuthPass,
       },
       tls: {
-        rejectUnauthorized: false, // Important if you have SSL/TLS issues
+        rejectUnauthorized: false,
       },
     });
 
-    // Read the email template file
-    const emailTemplate = fs.readFileSync(
-      "pages/emails/emailBody.html",
-      "utf8"
+    // Use path.join to ensure correct path in production
+    const emailTemplatePath = path.join(
+      process.cwd(),
+      "pages/emails/emailBody.html"
     );
 
-    // Compile the template
-    const compiledTemplate = handlebars.compile(emailTemplate);
-
-    // Replace placeholders with actual data
-    const replacedEmailBody = compiledTemplate({ name, email, message });
-
-    // Setup email data
-    const mailOptions = {
-      from: EmailFrom,
-      to: toEmail,
-      subject: nature,
-      html: replacedEmailBody,
-    };
-
     try {
-      // Send the email
+      const emailTemplate = fs.readFileSync(emailTemplatePath, "utf8");
+      const compiledTemplate = handlebars.compile(emailTemplate);
+      const replacedEmailBody = compiledTemplate({ name, email, message });
+
+      const mailOptions = {
+        from: EmailFrom,
+        to: toEmail,
+        subject: nature,
+        html: replacedEmailBody,
+      };
+
       await transporter.sendMail(mailOptions);
 
-      // Respond to the client
       res
         .status(200)
         .json({ success: true, message: "Email sent successfully" });
